8c8733ba57f79ded97d8d7a5dd3417324cb73677
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-04-12 14:17:22 +0300

    setup frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto flex flex-col items-center bg-sky-400">
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        hello
+      </button>
+    </div>
+  );
+}
