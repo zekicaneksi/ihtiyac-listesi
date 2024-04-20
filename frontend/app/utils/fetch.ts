59add125dfb53b8df5aa56555480d15fe94258ff
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-04-20 17:24:23 +0300

    all fetch checks for authentication and redirects to /sign

--- frontend/app/utils/fetch.ts
+++ frontend/app/utils/fetch.ts
@@ -1,18 +1,32 @@
 let backendUrlPrefix = "/api";
 
-export function fetchBackendPOST<BodyType>(
+// Check response for usual stuff such as 401 for redirecting to /sign
+function processResponse(res: Response) {
+  if (res.status === 401) {
+    window.location.replace("/sign");
+  }
+}
+
+export async function fetchBackendPOST<BodyType>(
   url: string,
   bodyData: BodyType,
 ): Promise<Response> {
-  return fetch(backendUrlPrefix + url, {
+  const response: Response = await fetch(backendUrlPrefix + url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(bodyData),
   });
+
+  processResponse(response);
+
+  return response;
 }
 
-export function fetchBackendGET(url: string): Promise<Response> {
-  return fetch(backendUrlPrefix + url);
+export async function fetchBackendGET(url: string): Promise<Response> {
+  const response: Response = await fetch(backendUrlPrefix + url);
+  processResponse(response);
+
+  return response;
 }
