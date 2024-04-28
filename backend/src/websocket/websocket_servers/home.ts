0526a5824666ad236d184584e79a667d01e1211e
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-04-28 17:23:37 +0300

    keeping websocket connections in a map

--- backend/src/websocket/websocket_servers/home.ts
+++ backend/src/websocket/websocket_servers/home.ts
@@ -4,16 +4,19 @@ import { WebSocket, WebSocketServer } from "ws";
 
 const wsServer = new WebSocketServer({ noServer: true });
 
+const connectionMap = new Map<Session, { ws: WebSocket; user: User }>();
+
 wsServer.on(
   "connection",
   function connection(ws: WebSocket, session: Session, user: User) {
+    connectionMap.set(session, { ws: ws, user: user });
+
     ws.on("error", console.error);
-
-    console.log(session);
-    console.log(user);
-
     ws.on("message", function message(data) {
       console.log("received: %s", data);
+    });
+    ws.on("close", function close() {
+      connectionMap.delete(session);
     });
 
     ws.send("something");
