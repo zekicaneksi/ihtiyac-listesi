23077c1cf546359e1769277b8dfc28969152e6e7
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-04-28 18:57:33 +0300

    ping ponging to detect broken connections in the backend

--- backend/src/websocket/websocket_servers/home.ts
+++ backend/src/websocket/websocket_servers/home.ts
@@ -6,21 +6,42 @@ const wsServer = new WebSocketServer({ noServer: true });
 
 const connectionMap = new Map<Session, { ws: WebSocket; user: User }>();
 
+interface CustomWebSocket extends WebSocket {
+  isAlive: boolean;
+}
+
 wsServer.on(
   "connection",
-  function connection(ws: WebSocket, session: Session, user: User) {
-    connectionMap.set(session, { ws: ws, user: user });
-
+  function connection(ws: CustomWebSocket, session: Session, user: User) {
+    ws.isAlive = true;
     ws.on("error", console.error);
-    ws.on("message", function message(data) {
-      console.log("received: %s", data);
+    ws.on("pong", () => {
+      ws.isAlive = true;
     });
     ws.on("close", function close() {
       connectionMap.delete(session);
     });
+    ws.on("message", function message(data) {
+      console.log("received: %s", data);
+    });
+
+    connectionMap.set(session, { ws: ws, user: user });
 
     ws.send("something");
   },
 );
+
+const interval = setInterval(function ping() {
+  (wsServer.clients as Set<CustomWebSocket>).forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
+wsServer.on("close", function close() {
+  clearInterval(interval);
+});
 
 export default wsServer;
