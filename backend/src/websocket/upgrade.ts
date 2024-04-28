71e33620c4f84edd127fc2e39310e310c8e031bc
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-04-28 17:10:19 +0300

    authenticating when upgrading websocket connections

--- backend/src/websocket/upgrade.ts
+++ backend/src/websocket/upgrade.ts
@@ -1,20 +1,78 @@
 import internal from "stream";
 import wssHome from "./websocket_servers/home";
 import { IncomingMessage } from "http";
+import cookie from "cookie";
+import dbCon from "@/setup/database/db_setup";
+import { Session } from "@/setup/database/collections/sessions";
+import { ObjectId } from "mongodb";
+import { User } from "@/setup/database/collections/users";
+
+async function authenticateUser(request: IncomingMessage): Promise<{
+  user: User | null;
+  session: Session | null;
+  status: "unauthenticated" | "internalerr" | "authenticated";
+}> {
+  let cookies = cookie.parse(request.headers.cookie || "");
+  let sessionId: string = cookies.sessionid;
+
+  if (!sessionId) {
+    return { user: null, session: null, status: "unauthenticated" };
+  }
+
+  let session = await dbCon
+    .collection<Session>("sessions")
+    .findOne({ _id: new ObjectId(sessionId) });
+
+  if (!session) {
+    return { user: null, session: null, status: "unauthenticated" };
+  }
+
+  let user = await dbCon
+    .collection<User>("users")
+    .findOne({ _id: session.user_id });
+
+  if (!user) {
+    return { user: null, session: session, status: "internalerr" };
+  }
+
+  return { user: user, session: session, status: "authenticated" };
+}
+
+function onSocketError(err: Error) {
+  console.error(err);
+}
 
 async function upgrade(
   request: IncomingMessage,
   socket: internal.Duplex,
   head: Buffer,
 ) {
-  const pathname = request.url;
+  socket.on("error", onSocketError);
 
-  if (pathname === "/ws/") {
-    wssHome.handleUpgrade(request, socket, head, function done(ws) {
-      wssHome.emit("connection", ws, request);
-    });
-  } else {
+  function rejectConnection(msg: string) {
+    socket.write(msg);
     socket.destroy();
+  }
+
+  // Authenticating user
+  const { user, session, status } = await authenticateUser(request);
+
+  if (status === "unauthenticated") {
+    rejectConnection("HTTP/1.1 401 Unauthorized\r\n\r\n");
+  } else if (status === "internalerr") {
+    rejectConnection("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+  } else {
+    // Upgrade Request
+    socket.removeListener("error", onSocketError);
+    const pathname = request.url;
+
+    if (pathname === "/ws/") {
+      wssHome.handleUpgrade(request, socket, head, function done(ws) {
+        wssHome.emit("connection", ws, session, user);
+      });
+    } else {
+      socket.destroy();
+    }
   }
 }
 
--- backend/src/websocket/websocket_servers/home.ts
+++ backend/src/websocket/websocket_servers/home.ts
@@ -1,15 +1,23 @@
-import { WebSocketServer } from "ws";
+import { Session } from "@/setup/database/collections/sessions";
+import { User } from "@/setup/database/collections/users";
+import { WebSocket, WebSocketServer } from "ws";
 
 const wsServer = new WebSocketServer({ noServer: true });
 
-wsServer.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+wsServer.on(
+  "connection",
+  function connection(ws: WebSocket, session: Session, user: User) {
+    ws.on("error", console.error);
 
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
+    console.log(session);
+    console.log(user);
 
-  ws.send("something");
-});
+    ws.on("message", function message(data) {
+      console.log("received: %s", data);
+    });
+
+    ws.send("something");
+  },
+);
 
 export default wsServer;
