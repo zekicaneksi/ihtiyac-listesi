8f8ca26a337b3bba7cc5538da693d65c95054367
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-05-02 21:03:51 +0300

    profile page layout

--- frontend/app/(app)/profile/page.tsx
+++ frontend/app/(app)/profile/page.tsx
@@ -2,8 +2,12 @@
 
 import { fetchBackendGET } from "@/app/utils/fetch";
 import { useRouter } from "next/navigation";
+import Footer, { MenuElementProps } from "@/app/(app)/components/layout/footer";
+import { useUserContext } from "@/app/context/user_context";
+import Button from "@/app/components/Button";
 
 const Profile = () => {
+  const { user } = useUserContext();
   const router = useRouter();
 
   async function logout() {
@@ -11,13 +15,28 @@ const Profile = () => {
     router.push("/sign");
   }
 
+  const menuElements: MenuElementProps[] = [
+    {
+      text: "Logout",
+      onClick: () => {
+        logout();
+      },
+    },
+  ];
+
   return (
-    <button
-      className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
-      onClick={logout}
-    >
-      logout
-    </button>
+    <>
+      <div className="flex flex-grow flex-col items-center justify-center gap-10 [&>p]:text-center">
+        <div className="flex w-fit flex-col gap-8">
+          <div className="flex flex-row justify-center gap-8">
+            <div className="size-16 bg-red-300"></div>
+            <p className="self-center">{user.fullname}</p>
+          </div>
+          <Button>Upload Picture</Button>
+        </div>
+      </div>
+      <Footer menuElements={menuElements} />
+    </>
   );
 };
 
