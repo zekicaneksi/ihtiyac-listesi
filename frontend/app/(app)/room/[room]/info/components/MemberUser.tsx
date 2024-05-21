cd8f3bef06ff169a81e6bfe8ff317ce678b385b2
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-05-21 19:49:41 +0300

    split the info page into components

---
+++ frontend/app/(app)/room/[room]/info/components/MemberUser.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { User } from "@/app/(app)/context/user_context";
+import ProfilePicture from "@/app/components/ProfilePicture";
+import { useState } from "react";
+import { TiDeleteOutline } from "react-icons/ti";
+import RemoveMemberPopup from "./RemoveMemberPopup";
+
+export interface MemberUserProps {
+  user: User;
+  isAdmin: boolean;
+  myId: string;
+}
+
+const MemberUser = (props: MemberUserProps) => {
+  const [showRemoveMemberPopup, setShowRemoveMemberPopup] =
+    useState<boolean>(false);
+
+  function handleRemoveMemberPopupClose() {
+    setShowRemoveMemberPopup(false);
+  }
+
+  return (
+    <>
+      <RemoveMemberPopup
+        memberId={props.user._id}
+        memberName={props.user.fullname}
+        isOpen={showRemoveMemberPopup}
+        handleClose={handleRemoveMemberPopupClose}
+      />
+      <div
+        className={`relative flex flex-col border-b-2 border-black bg-element p-4`}
+      >
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row items-center gap-2">
+            <ProfilePicture address={props.user.profilePictureId} />
+            <p>{props.user.fullname}</p>
+          </div>
+          {props.isAdmin && props.user._id !== props.myId && (
+            <div className="self-center">
+              <TiDeleteOutline
+                className="size-8 text-foreground hover:cursor-pointer hover:brightness-150"
+                onClick={() => {
+                  setShowRemoveMemberPopup(true);
+                }}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MemberUser;
---
+++ frontend/app/(app)/room/[room]/info/components/RemoveMemberPopup.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Popup from "@/app/components/Popup";
+import { fetchBackendPOST } from "@/app/utils/fetch";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface RemoveMemberPopupProps {
+  memberId: string;
+  memberName: string;
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+function RemoveMemberPopup(props: RemoveMemberPopupProps) {
+  const [infoMessage, setInfoMessage] = useState<string>("");
+  const [disableForm, setDisableForm] = useState<boolean>(false);
+
+  const pathname = usePathname();
+
+  function handlePopupClose() {
+    if (!disableForm) props.handleClose();
+  }
+
+  useEffect(() => {
+    setInfoMessage("");
+  }, [props.isOpen]);
+
+  async function handleRemoveBtn() {
+    setDisableForm(true);
+    setInfoMessage("removing...");
+
+    interface PostData {
+      roomId: string;
+      memberId: string;
+    }
+
+    const response = await fetchBackendPOST<PostData>("/remove-member", {
+      roomId: pathname.split("/")[2],
+      memberId: props.memberId,
+    });
+
+    if (response.status === 200) {
+      handlePopupClose();
+    } else {
+      setInfoMessage("Something went wrong!");
+    }
+
+    setDisableForm(false);
+  }
+
+  function handleCancelBtn() {
+    handlePopupClose();
+  }
+
+  return (
+    <Popup open={props.isOpen} handleClose={handlePopupClose}>
+      <div
+        className={`${disableForm ? "pointer-events-none opacity-70" : ""} m-auto flex w-80  flex-col gap-4 bg-foreground px-4 py-4`}
+      >
+        <p className="text-center">
+          Are you sure you want to remove member <b>{props.memberName}</b>?
+        </p>
+        {infoMessage && <p className="text-center">{infoMessage}</p>}
+        <Button onClick={handleRemoveBtn}>Remove</Button>
+        <Button onClick={handleCancelBtn}>Cancel</Button>
+      </div>
+    </Popup>
+  );
+}
+
+export default RemoveMemberPopup;
