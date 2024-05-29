ac8c004d205eda40de492c2f454b307e7e08d7e1
Zeki Can EKSI <zekicaneksi@gmail.com>
2024-05-29 19:33:33 +0300

    using Link instead of router push in room listings

--- frontend/app/(app)/components/Room.tsx
+++ frontend/app/(app)/components/Room.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Snackbar from "@/app/components/Snackbar/Snackbar";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { useState } from "react";
 import { FaCopy } from "react-icons/fa";
 
@@ -16,8 +16,6 @@ interface RoomProps {
 
 const Room = (props: RoomProps) => {
   const [isSnackbarOpen, setIsSnackbarOpen] = useState<boolean>(false);
-
-  const router = useRouter();
 
   async function copyRoomId(e: React.MouseEvent<HTMLDivElement>) {
     e.stopPropagation();
@@ -34,29 +32,26 @@ const Room = (props: RoomProps) => {
         }}
         open={isSnackbarOpen}
       />
-      <div
-        className="relative flex h-24 items-center justify-center border-b-2 border-foreground bg-element hover:cursor-pointer hover:brightness-110"
-        onClick={() => {
-          router.push(`/room/${props.room.roomId}`);
-        }}
-      >
-        <p className="flex-grow text-center sm:flex-grow-0">
-          {props.room.roomName}
-        </p>
-        <div
-          className="group right-0 flex h-full flex-col justify-between p-3 sm:absolute"
-          onClick={copyRoomId}
-        >
-          <div className="flex flex-grow flex-col items-center justify-center">
-            <FaCopy className="size-8 fill-foreground group-hover:fill-background" />
-          </div>
-          <div className="flex flex-grow flex-col justify-end">
-            <p className="text-foreground group-hover:text-background">
-              {props.room.roomId}
-            </p>
+      <Link href={`/room/${props.room.roomId}`}>
+        <div className="relative flex h-24 items-center justify-center border-b-2 border-foreground bg-element hover:cursor-pointer hover:brightness-110">
+          <p className="flex-grow text-center sm:flex-grow-0">
+            {props.room.roomName}
+          </p>
+          <div
+            className="group right-0 flex h-full flex-col justify-between p-3 sm:absolute"
+            onClick={copyRoomId}
+          >
+            <div className="flex flex-grow flex-col items-center justify-center">
+              <FaCopy className="size-8 fill-foreground group-hover:fill-background" />
+            </div>
+            <div className="flex flex-grow flex-col justify-end">
+              <p className="text-foreground group-hover:text-background">
+                {props.room.roomId}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
